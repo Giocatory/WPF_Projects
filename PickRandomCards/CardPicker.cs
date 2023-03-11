d9457f6820d9808b514923f55849f4c651cd0bd3
Giocatory <nivolyas@gmail.com>
2023-03-11 18:25:52 +0300

    11.03.23

---
+++ PickRandomCards/CardPicker.cs
@@ -0,0 +1,41 @@
+﻿namespace PickRandomCards
+{
+    internal class CardPicker
+    {
+        private static readonly Random random = new Random();
+
+        public static string[] PickSomeCards(int numberOfCards)
+        {
+            string[] pickedCards = new string[numberOfCards];
+            for (int i = 0; i < numberOfCards; i++)
+            {
+                pickedCards[i] = RandomValue() + " " + RandomSuit();
+            }
+            return pickedCards;
+        }
+
+        private static string RandomSuit()
+        {
+            return random.Next(1, 5) switch
+            {
+                1 => "Пики",
+                2 => "Черви",
+                3 => "Крести",
+                _ => "Бубны"
+            };
+        }
+
+        private static string RandomValue()
+        {
+            int value = random.Next(1, 14);
+            return value switch
+            {
+                1 => "Туз",
+                11 => "Валет",
+                12 => "Дама",
+                13 => "Король",
+                _ => value.ToString()
+            };
+        }
+    }
+}
---
+++ PickRandomCards/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace PickRandomCards
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Введите количество карт: ");
+            string? line = Console.ReadLine();
+
+            int numberOfCards = int.TryParse(line, out int _) ? Convert.ToInt32(line) : 0;
+
+            if (numberOfCards == 0)
+            {
+                Console.WriteLine("Вы ввели 0 или неправильное значение!");
+            }
+            else
+            {
+                string[] cards = CardPicker.PickSomeCards(numberOfCards);
+                foreach (string card in cards)
+                {
+                    Console.WriteLine($"{card};");
+                }
+            }
+        }
+    }
+}
