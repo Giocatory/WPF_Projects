3b13977a5347fcfca1b851d38c5bfe26f1075c76
Giocatory <nivolyas@gmail.com>
2023-03-12 15:01:03 +0300

    12.03.23

--- GuysCash/Guy.cs
+++ GuysCash/Guy.cs
@@ -1,12 +1,68 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GuysCash
+﻿namespace GuysCash
 {
     internal class Guy
     {
+        public string Name { get; set; }
+        public int Cash { get; set; }
+
+        // Цепочка констуркторов
+        public Guy() : this(string.Empty, 0) { }
+
+        public Guy(string name) : this(name, 0) { }
+
+        public Guy(string name, int cash)
+        {
+            this.Name = name;
+            this.Cash = cash;
+        }
+
+        /// <summary>
+        /// Вывод имени и количества денег
+        /// </summary>
+        public void WriteMyInfo()
+        {
+            Console.WriteLine($"{Name} has {Cash} bucks.");
+        }
+
+        /// <summary>
+        /// Выдает часть моих денег, удаляя их из кармана (или выводит на консоль
+        /// сообщение о том, что денег недостаточно).
+        /// </summary>
+        /// <param name="amount">Выдаваемая сумма.</param>
+        /// <returns>
+        /// Сумма денег, взятая из кармана, или 0, если денег не хватает
+        /// (или если сумма недействительна).
+        /// </returns>
+        public int GiveCash(int amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine($"{Name} says: {amount} isn't a valid amount");
+            }
+            if (amount > Cash)
+            {
+                Console.WriteLine($"{Name} says: I don't have enough cash to give you {amount}");
+                return 0;
+            }
+            Cash -= amount;
+            return amount;
+        }
+
+        /// <summary>
+        /// Получает деньги, добавляя их в мой карман (или выводит
+        /// сообщение на консоль, если сумма недействительна).
+        /// </summary>
+        /// <param name="amount">Получаемая  сумма.</param>
+        public void ReceiveCash(int amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine($"{Name} says: {amount} isn't an amount I'll take");
+            }
+            else
+            {
+                Cash += amount;
+            }
+        }
     }
 }
--- GuysCash/Program.cs
+++ GuysCash/Program.cs
@@ -2,9 +2,55 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var joe = new Guy()
+            {
+                Name = "Joe",
+                Cash = 50
+            };
+            var bob = new Guy("Bob", 100);
+
+            while (true)
+            {
+                // Вызовите методы WriteMyInfo для каждого объекта Guy
+                joe.WriteMyInfo();
+                bob.WriteMyInfo();
+
+                Console.Write("Enter an amount: ");
+                string? howMuch = Console.ReadLine();
+
+                if (howMuch == "") return;
+                // Используйте метод int.TryParse для преобразования строки howMuch в int
+                // (как это было сделано ранее в этой главе)
+                if (int.TryParse(howMuch, out int cash))
+                {
+                    Console.Write("Who should give the cash: ");
+                    string? whichGuy = Console.ReadLine();
+                    if (whichGuy == "Joe")
+                    {
+                        // Вызовите метод GiveCash объекта joe и сохраните результат
+                        joe.GiveCash(cash);
+                        // Вызовите метод ReceiveCash объекта bob с сохраненным результатом
+                        bob.ReceiveCash(cash);
+                    }
+                    else if (whichGuy == "Bob")
+                    {
+                        // Вызовите метод GiveCash объекта bob и сохраните результат
+                        bob.GiveCash(cash);
+                        // Вызовите метод GiveCash объекта joe с сохраненным результатом
+                        joe.ReceiveCash(cash);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter 'Joe' or 'Bob'");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please enter an amount (or a blank line to exit).");
+                }
+            }
         }
     }
 }
