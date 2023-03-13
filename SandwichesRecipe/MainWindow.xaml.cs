a84b0b7c4b02deb0f7a827246e233fd2444d5059
Giocatory <nivolyas@gmail.com>
2023-03-13 17:46:53 +0300

    13.03.23

--- SandwichesRecipe/MainWindow.xaml.cs
+++ SandwichesRecipe/MainWindow.xaml.cs
@@ -23,6 +23,56 @@ namespace SandwichesRecipe
         public MainWindow()
         {
             InitializeComponent();
+            MakeTheMenu();
+        }
+
+        private void MakeTheMenu()
+        {
+            MenuItem[] menuItems = new MenuItem[6];
+            string guacomolePrice = string.Empty;
+
+            for (int i = 0; i < menuItems.Length; i++)
+            {
+                menuItems[i] = new MenuItem();
+                if (i >= 3)
+                {
+                    menuItems[i].Breads = new string[] { "Авиационный Бублик", "Луковый бублик", "Бублик пампушка", "Бублик" };
+                }
+                menuItems[i].Generate();
+            }
+
+            item1.Text = menuItems[0].Description;
+            price1.Text = menuItems[0].Price;
+
+            item2.Text = menuItems[1].Description;
+            price2.Text = menuItems[1].Price;
+
+            item3.Text = menuItems[2].Description;
+            price3.Text = menuItems[2].Price;
+
+            item4.Text = menuItems[3].Description;
+            price4.Text = menuItems[3].Price;
+
+            item5.Text = menuItems[4].Description;
+            price5.Text = menuItems[4].Price;
+
+            MenuItem specialMenuItem = new MenuItem() 
+            {
+                Proteins = new string[] { "Органическая ветчина", "Грибная котлета", "Мортаделла" },
+                Breads = new string[] { "ролл без глютена", "пита", "лаваш" },
+                Condiments = new string[] { "дижонская горчица", "соус мисо", "сок" }
+            };
+            specialMenuItem.Generate();
+
+            item6.Text = specialMenuItem.Description;
+            price6.Text = specialMenuItem.Price;
+
+            MenuItem guacomoleMenuItem = new MenuItem();
+            guacomoleMenuItem.Generate();
+            guacomolePrice = guacomoleMenuItem.Price;
+
+            guacamole.Text = $"Add guacamole for {guacomoleMenuItem.Price}";
+
         }
     }
 }
---
+++ SandwichesRecipe/MenuItem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SandwichesRecipe
+{
+    class MenuItem
+    {
+        public Random Randomizer = new();
+
+        // Белки
+        public string[] Proteins = { "Жареная говядина", "Салями", "Индейка", "Ветчина", "Макароны", "Тофу" };
+        // Приправы
+        public string[] Condiments = { "Желтая горчица", "Коричневая горчица", "Медовая горчица", "Майонез", "Соевый соус", "Сырный соус" };
+        // Хлебы
+        public string[] Breads = { "Ржаной", "Белый", "Пшеничный", "Пампушка", "Багет" };
+
+        public string Description = string.Empty;
+        public string Price = string.Empty;
+
+        public void Generate()
+        {
+            string randomProtein = Proteins[Randomizer.Next(Proteins.Length)];
+            string randomCondiments = Condiments[Randomizer.Next(Condiments.Length)];
+            string randomBread = Breads[Randomizer.Next(Breads.Length)];
+            Description = $"{randomProtein} с {randomCondiments} на {randomBread}";
+
+            decimal bucks = Randomizer.Next(2, 5);
+            decimal cents = Randomizer.Next(1, 98);
+            decimal price = bucks + (cents * .01m);
+            Price = price.ToString("c"); // "с" для вывода локальной валюты
+        }
+    }
+}
