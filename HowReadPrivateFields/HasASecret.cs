b14a93ab6bee7771ba93c0c067c0771d625866f4
Giocatory <nivolyas@gmail.com>
2023-03-15 04:57:36 +0300

    read private values

---
+++ HowReadPrivateFields/HasASecret.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HowReadPrivateFields
+{
+    internal class HasASecret
+    {
+        private readonly string secret = "secret phraze";
+        private int lengthSecretPhraze = 5;
+    }
+}
---
+++ HowReadPrivateFields/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+
+namespace HowReadPrivateFields
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HasASecret keeper = new();
+
+            FieldInfo[] fields = keeper.GetType().GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
+
+            foreach (FieldInfo field in fields)
+            {
+                Console.WriteLine($"Variable name: {field.Name}; Variable value: {field.GetValue(keeper)}");
+            }
+            // Variable name: secret; Variable value: secret phraze
+            // Variable name: lengthSecretPhraze; Variable value: 5
+        }
+    }
+}
