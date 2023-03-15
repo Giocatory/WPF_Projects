91a61f3d2ce9d8ab34305566ada37dc7641d9705
Giocatory <nivolyas@gmail.com>
2023-03-15 07:37:35 +0300

    15.03.23

--- WeaponsFireInheritance/Program.cs
+++ WeaponsFireInheritance/Program.cs
@@ -4,8 +4,8 @@
     {
         private static void Main(string[] args)
         {
-            Arrow smallArrow = new Arrow("Маленький лук", "дальний бой");
-            Sword claymore = new Sword("Двуручный меч", "Ближний бой");
+            var smallArrow = new Arrow("Маленький лук", "дальний бой");
+            var claymore = new Sword("Двуручный меч", "Ближний бой");
 
             Console.WriteLine($"{smallArrow.GetType().Name} ({smallArrow.Title}) - {smallArrow.Description}. Звук: {smallArrow.FireVoice()}. Damage - {smallArrow.WeaponDamage()}");
             Console.WriteLine($"{claymore.GetType().Name} ({claymore.Title}) - {claymore.Description}. Звук: {claymore.FireVoice()}. Damage - {claymore.WeaponDamage()}");
